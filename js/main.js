1f9da5390fdd680ab5847fba9522469536ff7773
Jane72-boop <71617423+Jane72-boop@users.noreply.github.com>
2022-07-18 11:45:29 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-import info from "/data.json" assert { type: "json" };
+let info = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
 
 let labels = info.map((a) => a.day);
 let amounts = info.map((a) => a.amount);
